f5fb2d48904dc449e6208e83b472c85f700e37d7
hayk4500 <108433596+hayk4500@users.noreply.github.com>
2022-07-31 22:37:09 +0300

    Initial commit.

---
+++ ArrayTest/ArrayTest.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int const MAX = 5;
+	int array [MAX];
+	for (int i = 0; i < MAX; i++) {
+		cout << "Enter index(" << i << "): ";
+		cin >> array[i];
+	}
+	for (int k = 0; k < MAX; k++) {
+		cout << endl;
+		cout << "Enters numbers in index " << k << " is: " << array[k];
+	}
+	return 0;
+}
